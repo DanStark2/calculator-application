f18ae0f87d0457e07e397625dfd80aaeee7fa754
Dan Stark <140963025+DanStark2@users.noreply.github.com>
2026-04-13 14:47:26 +0200

    first commit

---
+++ fakultet.js
@@ -0,0 +1,7 @@
+export function fakultet(tall) {
+    if (typeof tall !== 'number') return 1;
+    if (tall <= 0) return 1;
+    let result = 1;
+    for (let i = 2; i <= tall; i++) result *= i;
+    return result;
+}
