0515de27d6d407c93eb598af154d512509493b28
Dan Stark <140963025+DanStark2@users.noreply.github.com>
2026-04-27 14:50:06 +0200

    Introduce bug in fakultet function

--- fakultet.js
+++ fakultet.js
@@ -2,6 +2,6 @@ export function fakultet(tall) {
     if (typeof tall !== 'number') return 1;
     if (tall <= 0) return 1;
     let result = 1;
-    for (let i = 2; i <= tall; i++) result *= i;
+    for (let i = 2; i < tall; i++) result *= i;
     return result;
 }
